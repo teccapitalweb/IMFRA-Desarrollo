e02d7cc8b5b46490c1e31661ba2459cc83b8213a
TEC Capital web <teccapitalweb@gmail.com>
2026-09-18 16:05:44 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
    Sube este archivo a la raíz del repositorio, junto a index.html.
    ============================================================ */
 
-var CACHE = 'imfra-vip-v19';
+var CACHE = 'imfra-vip-v20';
 
 // Al instalar, activarse de inmediato (sin esperar a que se cierren pestañas viejas)
 self.addEventListener('install', function (e) {
